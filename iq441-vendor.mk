e02f021f84842d0e6d2bb7739cdbe9295a193136
JaMByDev <jambydevs@gmail.com>
2016-05-10 16:19:07 +0300

    vendor release KK

---
+++ iq441-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/vendor,system/vendor)
