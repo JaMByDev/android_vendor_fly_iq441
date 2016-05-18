1012932b1e2dd26ad508489050b35a28a3737e28
JaMByDev <jambydevs@gmail.com>
2016-05-18 21:23:28 +0300

    fix

--- iq441-vendor.mk
+++ iq441-vendor.mk
@@ -4,8 +4,8 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/bin,syste
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/app,system/app)
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/xbin,system/xbin)
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/xbin,system/xbin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/fly/iq441/vendor,system/vendor)
